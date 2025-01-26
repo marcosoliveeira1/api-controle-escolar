f9d28ab0452019dc8820876a5e56698ca951d4d6
Marcos Oliveira <marcosoliveeira1@users.noreply.github.com>
2025-01-26 15:28:57 -0300

    initial commit

---
+++ src/main/java/com/platformbuilders/controleescolar/ControleEscolarApplication.java
@@ -0,0 +1,13 @@
+package com.platformbuilders.controleescolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleEscolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleEscolarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platformbuilders/controleescolar/ControleEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platformbuilders.controleescolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
