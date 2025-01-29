4d5584dbc0b2210924c46171d38e05b12ae183a9
Marcos Oliveira <marcosoliveeira1@users.noreply.github.com>
2025-01-28 21:46:58 -0300

    feat: add SchoolDTO

---
+++ src/main/java/com/platformbuilders/controleescolar/api/dto/SchoolDTO.java
@@ -0,0 +1,31 @@
+package com.platformbuilders.controleescolar.api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CNPJ;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SchoolDTO {
+    @NotBlank(message = "O campo 'name' é obrigatório")
+    private String name;
+
+    @NotBlank(message = "O campo 'cpnj' é obrigatório")
+    @CNPJ
+    private String cnpj;
+
+    @NotBlank(message = "O campo 'address' é obrigatório")
+    private String address;
+
+    @NotBlank(message = "O campo 'phone' é obrigatório")
+    private String phone;
+
+    @NotNull(message = "O campo 'studentsPerClassroom' é obrigatório")
+    @Min(value = 1, message = "O campo 'studentsPerClassroom' deve ser maior que zero")
+    private Integer studentsPerClassroom;
+}
