b6bc56e22637970f8d2d4bab2b7e6f0a0eb75033
Marcos Oliveira <marcosoliveeira1@users.noreply.github.com>
2025-02-01 00:32:19 -0300

    feat: add StudentService

---
+++ src/main/java/com/platformbuilders/controleescolar/student/service/StudentService.java
@@ -0,0 +1,65 @@
+package com.platformbuilders.controleescolar.student.service;
+
+
+import com.platformbuilders.controleescolar.student.api.dto.StudentDTO;
+import com.platformbuilders.controleescolar.student.domain.model.Student;
+import com.platformbuilders.controleescolar.student.exception.StudentNotFoundException;
+import com.platformbuilders.controleescolar.student.infra.repository.StudentRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class StudentService {
+    private final StudentRepository studentRepository;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository, ModelMapper modelMapper) {
+        this.studentRepository = studentRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    public StudentDTO create(StudentDTO studentDTO) {
+        Student student = modelMapper.map(studentDTO, Student.class);
+        Student savedStudent = studentRepository.save(student);
+
+        return modelMapper.map(savedStudent, StudentDTO.class);
+    }
+
+    public StudentDTO findById(Long id) {
+        Student student = studentRepository.findById(id)
+                .orElseThrow(StudentNotFoundException::new);
+
+        return modelMapper.map(student, StudentDTO.class);
+    }
+
+    public Page<StudentDTO> findAll(Pageable pageable) {
+        return studentRepository.findAll(pageable).map(student -> modelMapper.map(student, StudentDTO.class));
+    }
+
+    public StudentDTO update(Long id, StudentDTO studentDTO) {
+
+        Student existingStudent = studentRepository.findById(id)
+                .orElseThrow(StudentNotFoundException::new);
+
+        modelMapper.map(studentDTO, existingStudent);
+        existingStudent.setId(id);
+
+        Student updatedStudent = studentRepository.save(existingStudent);
+
+        return modelMapper.map(updatedStudent, StudentDTO.class);
+    }
+
+    public void delete(Long id) {
+       if (!studentRepository.existsById(id)) {
+           throw new StudentNotFoundException();
+       }
+
+       studentRepository.deleteById(id);
+    }
+}
